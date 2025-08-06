9d604f3eb72970b695eb2bf0b81e47cf92f3f2d2
LunchChickenBokkeumtang <hoon6481@gmail.com>
2025-08-06 12:43:31 +0900

    js 수정

--- js/script.js
+++ js/script.js
@@ -4,11 +4,13 @@
 $(document).on('click', 'a[href="#"]', e => e.preventDefault());
 
 $(function() {
-	$('.animate').scrolla({
-		mobile: true, //모바일버전시 활성화
-		once: false //스크롤시 딱 한번만 하고싶을땐 true
-	});    
-      }); 
+  var isMobileView = window.matchMedia('(max-width: 500px)').matches;
+
+  $('.animate').scrolla({
+    mobile: true,
+    once: isMobileView
+  });
+});
 
 // --------------------------------------------------
 // [2] 실시간 시계 업데이트
